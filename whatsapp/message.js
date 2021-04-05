a0219180dd7ed5a9edd5b9f65f4d425ba0d6103e
yukino <71614812+ravel-iska@users.noreply.github.com>
2021-04-05 18:44:07 +0700

    Add files via upload

---
+++ whatsapp/message.js
@@ -0,0 +1,10 @@
+const {
+	MessageType,
+	Mimetype,
+    WAConnection
+} = require("@adiwajshing/baileys");
+const fs = require('fs');
+const conn = require('./connect');
+const axios = require('axios');
+const request = require('request');
+
